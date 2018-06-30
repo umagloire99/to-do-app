a23d7b37c61e06a85617e9bcfdd4a2e90f78d4cd
umagloire99 <36057707+umagloire99@users.noreply.github.com>
2018-06-30 10:58:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,20 +12,22 @@ while True:
     option = str(input("Type your choice: "))
     if option == '1':
         task = str(input('Enter your task: '))
-        if task.isidentifier():  # check if your task can be identified so is not empty
+        if task:  # check if your task can be identified so is not empty
             task = do.setTask(task)
             status = str(input('Enter the status of your task (Done or Not done): '))
             do.setStatus(status)
-            Date = task_date = str(input("Enter the date that you will execute your task (Eg: DD/MM/YY): "))
+            Date = task_date = str(input("Enter the date that you will execute your task (Eg: DD/MM/YYYY): "))
             do.setDate(Date)
             time_task = str(input("Enter the time that you will execute your task (Eg: HH:MM PM): "))
             do.setTime(time_task)
             do.Store()
         else:
-            print('We cannot save this task!')
+            print('***We cannot save this task!***')
+            print('you have to enter a word or sentence before save your task\n')
 
     elif option == '2':
         do.display()
+        
 
     elif option == '3':
         do.display()
